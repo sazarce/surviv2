8a7b6ca9cc3f854fde8e6fa64c6a378fa5723f39
w3x731 <mredwardprise@gmail.com>
2018-05-11 11:01:16 +0300

    Update app client version b9dc574e

--- patcher/index.js
+++ patcher/index.js
@@ -21,7 +21,7 @@ function safePatch(name, options) {
 
 var aimScriptPath = "../survivIoAim.js";
 
-var appPrint = "0ca5c0d7";
+var appPrint = "b9dc574e";
 var appFolderPath = "../app/";
 var appPath = appFolderPath + "app." + appPrint + ".js";
 var patchedAppPath = appFolderPath + "patched/app." + appPrint + ".js";
@@ -31,31 +31,184 @@ var aimExtensionFolderPath = "../survivIoAimChromeExtension/"
 fs.createReadStream(appPath).pipe(fs.createWriteStream(patchedAppPath)).on('finish', function() {
 	var scopeOptions = {
 		files: patchedAppPath,
-		from: /this.activePlayer=null,/g,
-		to: 'this.activePlayer=null,game=this,',
-	};
-
-	var alphaOptions = {
-		files: patchedAppPath,
-		from: /alpha:1/g,
-		to: 'alpha:0.75',
+		from: /this\.activePlayer=null\,/g,
+		to: 'this\.activePlayer=null\,game=this\,',
 	};
 
 	var smokeAlpha = {
 		files: patchedAppPath,
-		from: /d.sprite.alpha=u/g,
-		to: 'd.sprite.alpha=0.1',
+		from: /m\.sprite\.alpha=u/g,
+		to: 'm\.sprite\.alpha=0.1'
 	};
 
 	var bushAlpha = {
 		files: patchedAppPath,
-		from: /alpha:.97/g,
-		to: 'alpha:0.5',		
+		from: /alpha\:\.97/g,
+		to: 'alpha\:0.5'
 	};
 
+	var tree_01Alpha = {
+		files: patchedAppPath,
+		from: /residue\:\"img\/map\/map\-tree\-res\.svg\"\,scale\:\.7\,alpha\:1/g,
+		to: 'residue:"img/map/map-tree-res.svg",scale:.7,alpha:0.5'
+	}
+
+	var tree_02Alpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/map\-tree\-03\.svg\"\,residue\:\"img\/map\/map\-tree\-res\.svg\"\,scale\:1\,alpha\:1/g,
+		to: 'sprite:"img/map/map-tree-03.svg",residue:"img/map/map-tree-res.svg",scale:1,alpha:0.5'
+	}
+
+	var shackAlpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/map\-building\-shack\-ceiling\.svg\"\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/map-building-shack-ceiling.svg",scale:.5,alpha:0.1'
+	}
+
+	var outhouseAlpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/map\-building\-outhouse\-ceiling\.svg\"\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/map-building-outhouse-ceiling.svg",scale:.5,alpha:0.5'
+	}
+
+	var panicroomAlpha = {
+		files: patchedAppPath,
+		from: /sprite:"img\/map\/map\-building\-panicroom\-ceiling\.svg\"\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/map-building-panicroom-ceiling.svg",scale:.5,alpha:0.5'
+	}
+
+	var barn_01Alpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/map\-building\-barn\-ceiling\.svg\"\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/map-building-barn-ceiling.svg",scale:.5,alpha:0.5'
+	}
+
+	var bank_01Alpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/map\-building\-bank\-ceiling\.svg\"\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/map-building-bank-ceiling.svg",scale:.5,alpha:0.5'
+	}
+
+	var vault_01Alpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/map\-building\-vault\-ceiling\.svg\"\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/map-building-vault-ceiling.svg",scale:.5,alpha:0.5'
+	}
+
+	var police_01Alpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/map\-building\-police\-ceiling\.svg\"\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/map-building-police-ceiling.svg",scale:.5,alpha:0.5'
+	}
+
+	var house_red_01Alpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/map\-building\-house\-ceiling\.svg\"\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/map-building-house-ceiling.svg",scale:.5,alpha:0.5'
+	}
+
+	var saferoom_01Alpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/map\-building\-saferoom\-ceiling\.svg\"\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/map-building-saferoom-ceiling.svg",scale:.5,alpha:0.5'
+	}
+
+	var mansion_01Alpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/map\-building\-mansion\-ceiling\.svg\"\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/map-building-mansion-ceiling.svg",scale:.5,alpha:0.5'
+	}
+
+	var bunker_egg_sublevel_01Alpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/map\-bunker\-egg\-chamber\-ceiling\-01\.svg\"\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/map-bunker-egg-chamber-ceiling-01.svg",scale:.5,alpha:0.5'	
+	}
+
+	var bunker_hydra_01Alpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/map\-bunker\-hydra\-ceiling\-01\.svg\"\,pos\:L\.create\(25\.25\,3\.5\)\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/map-bunker-hydra-ceiling-01.svg",pos:L.create(25.25,3.5),scale:.5,alpha:0.5'
+	}
+	
+	var bunker_hydra_sublevel_01Alpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/map\-bunker\-hydra\-chamber\-ceiling\-01\.svg\"\,pos\:L\.create\(7\,2\)\,scale\:\.5\,alpha\:1\,tint\:6250335\}\,\{sprite\:\"img\/map\/map\-bunker\-hydra\-chamber\-ceiling\-02\.svg\"\,pos\:L\.create\(\-13\.5\,\-76\.5\)\,scale\:\.5\,alpha\:1\,tint\:6250335\}\,\{sprite\:\"img\/map\/map\-bunker\-hydra\-chamber\-ceiling\-03\.svg\"\,pos\:L\.create\(38\,\-62\)\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/map-bunker-hydra-chamber-ceiling-01.svg",pos:L.create(7,2),scale:.5,alpha:0.5,tint:6250335},{sprite:"img/map/map-bunker-hydra-chamber-ceiling-02.svg",pos:L.create(-13.5,-76.5),scale:.5,alpha:0.5,tint:6250335},{sprite:"img/map/map-bunker-hydra-chamber-ceiling-03.svg",pos:L.create(38,-62),scale:.5,alpha:0.5'
+	}
+
+	var bunker_hydra_compartment_01Alpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/map\-bunker\-hydra\-compartment\-ceiling\-01\.svg\"\,pos\:L\.create\(0\,1\.25\)\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/map-bunker-hydra-compartment-ceiling-01.svg",pos:L.create(0,1.25),scale:.5,alpha:0.5'
+	}
+
+	var bunker_hydra_compartment_02Alpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/map\-bunker\-hydra\-compartment\-ceiling\-02\.svg\"\,pos\:L\.create\(0\,1\)\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/map-bunker-hydra-compartment-ceiling-02.svg",pos:L.create(0,1),scale:.5,alpha:0.5'
+	}
+
+	var bunker_hydra_compartment_03Alpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/map\-bunker\-hydra\-compartment\-ceiling\-03\.svg\"\,pos\:L\.create\(0\,1\)\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/map-bunker-hydra-compartment-ceiling-03.svg",pos:L.create(0,1),scale:.5,alpha:0.5'
+	}
+
+	var bunker_storm_01Alpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/map\-building\-shack\-ceiling\.svg\"\,pos\:L\.create\(-1\,10\)\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/map-building-shack-ceiling.svg",pos:L.create(-1,10),scale:.5,alpha:0.5'
+	}
+
+	var bunker_storm_sublevel_01Alpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/map\-bunker\-storm\-chamber\-ceiling\-01\.svg\"\,pos\:L\.create\(8\.5\,-1\)\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/map-bunker-storm-chamber-ceiling-01.svg",pos:L.create(8.5,-1),scale:.5,alpha:0.5'
+	}
+
+	var containerAlpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/\"\+e\.ceilingSprite\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/"+e.ceilingSprite,scale:.5,alpha:0.5'	
+	}
+
+	var warehouseAlpha = {
+		files: patchedAppPath,
+		from: /sprite\:\"img\/map\/map\-building\-warehouse\-ceiling\.svg\"\,scale\:\.5\,alpha\:1/g,
+		to: 'sprite:"img/map/"+e.ceilingSprite,scale:.5,alpha:0.5'
+	}
+
 	if(	safePatch("Scope", scopeOptions) && 
-		safePatch("Alpha", alphaOptions) && 
+		safePatch("Tree_01 alpha", tree_01Alpha) && 
+		safePatch("Tree_02 alpha", tree_02Alpha) &&
+		safePatch("Shack alpha", shackAlpha) &&
+		safePatch("Outhouse alpha", outhouseAlpha) &&
+		safePatch("Panicroom alpha", panicroomAlpha) &&
+		safePatch("Barn_01 alpha", barn_01Alpha) &&
+		safePatch("Bank_01 alpha", bank_01Alpha) &&
+		safePatch("Vault_01 alpha", vault_01Alpha) &&
+		safePatch("Police_01 alpha", police_01Alpha) &&
+		safePatch("House_red_01 alpha", house_red_01Alpha) &&
+		safePatch("Saferoom_01 alpha", saferoom_01Alpha) &&
+
+		safePatch("Mansion_01 alpha", mansion_01Alpha) &&
+		safePatch("Bunker_egg_sublevel_01 alpha", bunker_egg_sublevel_01Alpha) &&
+
+		safePatch("Bunker_hydra_01 alpha", bunker_hydra_01Alpha) &&
+		safePatch("Bunker_hydra_sublevel_01 alpha", bunker_hydra_sublevel_01Alpha) &&
+
+		safePatch("Bunker_hydra_compartment_01 alpha", bunker_hydra_compartment_01Alpha) &&
+		safePatch("Bunker_hydra_compartment_02 alpha", bunker_hydra_compartment_02Alpha) &&
+		safePatch("Bunker_hydra_compartment_03 alpha", bunker_hydra_compartment_03Alpha) &&
+
+		safePatch("Bunker_storm_01 alpha", bunker_storm_01Alpha) &&
+		safePatch("Bunker_storm_sublevel_01 alpha", bunker_storm_sublevel_01Alpha) &&
+
+		safePatch("Container alpha", containerAlpha) &&
+		safePatch("Warehouse alpha", warehouseAlpha) &&
 		safePatch("Smoke alpha", smokeAlpha) &&
+
 		safePatch("Bush alpha", bushAlpha)) {
 		// Modifying extension files if patched successfully
 		// Modifying aim extension
--- survivIoAimChromeExtension/background.js
+++ survivIoAimChromeExtension/background.js
@@ -2,13 +2,13 @@
 chrome.webRequest.onBeforeRequest.addListener(
 	function(details) {
 		return {
-			redirectUrl: chrome.runtime.getURL("js/app.0ca5c0d7.js")
+			redirectUrl: chrome.runtime.getURL("js/app.b9dc574e.js")
 		}
 	},
 	// filters
 	{
 		urls: [
-			"*://*.surviv.io/js/app.0ca5c0d7.js",
+			"*://*.surviv.io/js/app.b9dc574e.js",
 		],
 		types: ["script"]
 	},
