100590548e8363be725c13ba1bdc77119177b1c3
Zachary Deng <dengzac@gmail.com>
2018-06-08 02:43:02 -0700

    Fix headers

--- server/app.js
+++ server/app.js
@@ -1,18 +1,23 @@
 var http = require('http');
 var fs = require('fs')
 var path = require('path')
-var gunzip = require('zlib').createGunzip()
+var zlib = require('zlib')
 var mkdirp = require('mkdirp')
+var Readable = require('stream').Readable;
 http.createServer(onRequest).listen(3000);
 require('longjohn')
 
 function onRequest(client_req, client_res) {
 
     var response = client_res;
-    if (client_req.url != "/" && client_req.method != "POST" && fs.existsSync(path.join(__dirname, 'public', client_req.url))) {
+    if (!client_req.url.includes("api") && client_req.url != "/" && client_req.method != "POST" && fs.existsSync(path.join(__dirname, 'public', client_req.url))) {
         fs.readFile(path.join(__dirname, 'public', client_req.url), function(err, data) {
             //console.log("Cached " + client_req.url)
-            client_res.end(data);
+            fs.readFile(path.join(__dirname, 'public', client_req.url + ".headers"), function(err, data2){
+            	client_res.writeHead(200, JSON.parse(data2));
+            	client_res.end(data);
+            })
+            
         })
     } else {
         console.log('serve: ' + client_req.url);
@@ -35,27 +40,36 @@ function onRequest(client_req, client_res) {
                     port: 80,
                     path: client_req.url,
                     method: 'POST',
-                    headers: client_req.headers
+                    headers: client_req.headers,
+
                 };
                 post_options.headers.host = "surviv.io";
                 post_options.headers.origin = "http://surviv.io";
                 post_options.headers.referer = "http://surviv.io";
                 console.log(client_req.headers)
                 var post_req = http.request(post_options, function(res) {
-                    res.on('data', function(chunk) {
-                        console.log('Response ' + chunk.toString())
-                    })
+                	var gunzip = zlib.createGunzip();
+
                     var body = '';
                     res.pipe(gunzip)
                     gunzip.on('data', function(d){
                     	body += d;
                     })
                     gunzip.on('end', function(){
-                    	console.log(JSON.parse(body))
+                    	//console.log(res.headers);
+                    	console.log(body)
+
+                    	client_res.writeHead(200, res.headers)
+                    	
+                    	var s = new Readable();
+                    	s._read = function(){};
+                    	s.push(body);
+                    	s.push(null);
+                    	s.pipe(zlib.createGzip()).pipe(client_res);
                     })
                 })
-                post_req.write(queryData)
-                post_req.end()
+                post_req.write(queryData, function(e){post_req.end()})
+
             })
             //client_res..redirect(307, "http://surviv.io" + client_req.url);
             //console.log(client_req)
@@ -68,13 +82,18 @@ function onRequest(client_req, client_res) {
             });
             proxyRes.on('end', function() {
                 var buffer = Buffer.concat(data);
+                console.log(proxyRes.headers)
+                client_res.writeHead(200, proxyRes.headers);
                 client_res.end(buffer);
                 var fileName = path.join(__dirname, 'public', client_req.url);
                 mkdirp(path.dirname(fileName), function(err) {})
-                //console.log("Download " + fileName);
+                console.log("Download " + (fileName + ".headers"));
+                console.log(JSON.stringify(proxyRes.headers))
+
                 fs.writeFile(fileName, buffer, function(err) {
 
                 });
+                fs.writeFile(fileName + ".headers", JSON.stringify(proxyRes.headers), function(err){if (err) console.log(err)})
                 //console.log("Finish " + client_req.url)
             })
             proxyRes.on('error', function(e) { console.log("Exception " + JSON.stringify(e)) })
